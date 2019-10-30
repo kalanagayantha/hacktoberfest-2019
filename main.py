bc00b840831e3964bfe9696edbab3b8ee4d7ba25
kalanagayantha <43947400+kalanagayantha@users.noreply.github.com>
2019-10-30 17:50:12 +0530

    Add Comments

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-i = 0
-while i < 5:
+i = 0    #getting a variable call i
+while i < 5: #checking i is less than 5 
     print('*')
     if i % 2 == 0:
         print('**')
     if i > 2:
         print('***')
-    i = i + 1
+    i = i + 1   #increment i by 1 
